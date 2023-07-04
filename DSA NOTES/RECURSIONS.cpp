49fc7cd1404b37dbd759adac1f1e13a16a88f073
jopa1015161 <joshimarti.dc@gmail.com>
2023-07-05 01:02:06 +0530

    updated recursions

--- DSA NOTES/RECURSIONS.cpp
+++ DSA NOTES/RECURSIONS.cpp
@@ -67,9 +67,107 @@ int main() {
     int n;
     cin >> n;
     hal(n,n);
-    return 0;
+    returns 0;
 }
 
 
-//summation of numbers using recursion: 
+//summation of numbers using recursion using parameters: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i,int sum) {
+    if(i<1) {
+        cout << sum << endl;
+        return;
+    }
+    f(i-1,sum+i);
+    
+}
+
+int main() {
+    int i;
+    int n;
+    cout<< "enter the number: ";
+    cin >> n;
+    f(n,0);
+
+}
+
+//summation of N numbers using functional method:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n) {
+    if(n==0) {
+        return 0;
+    }return n + f(n-1); //if n=3--3+f(2)--3+2+f(1)--3+2+1+f(0) and here it returns zero and stops executing
+}
+int main() {
+    int n;
+    cout<< "enter the number: ";
+    cin >> n;
+    cout << f(n);
+
+}
+
+//recursion method for finding factorial using functional method:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n) {
+    if(n==0) {
+        return 1;
+    }return n*f(n-1);
+}
+int main() {
+    int i;
+    int n;
+    cout<< "enter the number: ";
+    cin >> n;
+    cout << f(n);
+
+}
+
+//reverse an ARRAY: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void  f(int i,int arr[],int n) {
+    if(i>=n/2) return;
+    swap(arr[i],arr[n-i-1]);
+    f(i+1,arr,n);
+}
+int main() {
+    int i;
+    int n;
+    cout<< "enter the size of array : ";
+    cin >>n;
+    int arr[n];
+    for (int i=0;i<n;i++) cin>> arr[i];
+    f(0,arr,n);
+    cout << "the reversed number is: " ;
+    for (int i=0;i<n;i++) cout << arr[i] << " ";
+    return 0;
+}
+
+//check if a string is a palindrome: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool f(int i,string &s) {
+    if(i>=s.size()/2) return true;
+    if(s[i]!=s[s.size()-i-1]) return false;
+    return f(i+1,s);
+}
+
+int main() {
+    string s = "madam";
+    cout << f(0,s);
+    return 0;
+}
 
