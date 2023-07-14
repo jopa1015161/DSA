9ba66e66232eb4bb6cf804019e0582e6d21cb94d
jopa1015161 <joshimarti.dc@gmail.com>
2023-07-14 16:35:39 +0530

    updated new datasheet problems

---
+++ DSA_DATASHEET/aray_easy.cpp
@@ -0,0 +1,33 @@
+//maximum and minimum number from an array: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void athi(int arr[],int n) {
+    for(int i=0;i<=n-2;i++) {
+        int mini = i;
+        for (int j = i;j<=n-1;j++) {
+            if(arr[j]< arr[mini]) mini = j;
+        }
+        swap(arr[mini],arr[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "enter the size of your array: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the elements of your array: ";
+    for(int i=0;i<n;i++) {
+        cin>> arr[i];
+    }
+    athi(arr,n);
+    
+    cout << endl << "min value: " << arr[0]<< endl;
+    cout << "max value: " << arr[n-1] ;
+
+    return 0;
+}
