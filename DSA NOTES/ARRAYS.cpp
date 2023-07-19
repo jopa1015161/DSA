08047af37f8b811e47444cbedfb23fdde00779ed
jopa1015161 <joshimarti.dc@gmail.com>
2023-07-19 16:09:10 +0530

    gfgarrays

--- DSA NOTES/ARRAYS.cpp
+++ DSA NOTES/ARRAYS.cpp
@@ -41,3 +41,58 @@ int main() {
 }
 -----------------------------------------------------------------------------------
 
+//insertion: 
+//Time Complexity: O(n) if we insert at the begining of the array and O(1) if we insert at the end of the array.
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int insert (int arr[],int n,int x, int pos,int cap) {
+    if(n == cap) {
+        return n;
+    }
+    else {
+        int idx = pos-1;
+        for(int i=n-1;i>=idx;i--) {
+            arr[i+1] = arr[i];
+            
+            
+        }
+        arr[idx] = x;
+        return (n+1);
+    }
+}
+    
+int main() {
+    int cap;
+    cout << "enter the capacity of your aray: ";
+    cin >> cap;
+    int n;
+    cout << "enter the size of your array: ";
+    cin >> n;
+    int arr[cap];
+    cout << "enter the elements: ";
+    for (int i=0;i<n;i++) {
+        cin>> arr[i] ;
+    }
+    
+    int x;
+    cout << "enter the element you wanna insert: ";
+    cin >> x;
+    
+    int pos;
+    cout << "enter the position of element you wanna insert: ";
+    cin >> pos;
+    
+    int newsize = insert(arr,n,x,pos,cap);
+    
+    cout << "array after insertion: ";
+    for (int i=0;i<newsize;i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+
+}
--- DSA_DATASHEET/ARRAY_EASY.cpp
+++ DSA_DATASHEET/ARRAY_EASY.cpp
@@ -1,4 +1,4 @@
-//maximum and minimum number from an array: 
+//maximum and minimum number from an array using selection sorting: 
 
 #include <bits/stdc++.h>
 
@@ -30,4 +30,42 @@ int main()
     cout << "max value: " << arr[n-1] ;
 
     return 0;
+}
+------------------------------------------------------------------------------------------------------
+
+//maximum and minimum value of any array using linear search: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void athi(int arr[],int n,int &mini,int &maxi) {
+    
+    maxi=max(arr[0],arr[1]);
+    mini=min(arr[0],arr[1]);
+    
+    for(int i=2;i<n;i++){
+        if(arr[i]>maxi) maxi=arr[i];
+        else if(arr[i]<mini) mini=arr[i];
+        
+    }
+}
+
+int main () {
+    int n;
+    cout << "enter the size of your function: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the elements of yor array: ";
+    for (int i=0; i<n;i++) {
+        cin >> arr[i];
+    }
+    int i;
+    int mini,maxi;
+    athi(arr,n,mini,maxi);
+    cout << "minimum value: "<< mini <<endl<<  "maximum value: " << maxi;
+    
+    return 0;
+    
 }
